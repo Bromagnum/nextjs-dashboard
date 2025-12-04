39a15e2307def2904cd02ea52d2fafa4a6305e66
Göktuğ Özfen <goktugozfen@gmail.com>
2025-12-04 16:12:54 +0300

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
+    // ❗ Build sırasında tüm TypeScript hatalarını yok say → Vercel’in TS validation bug’ını bypass eder
     ignoreBuildErrors: true,
   },
 };
