e2dd5f400f2892f3c7fd341821bdbc78d3ffc3a8
Göktuğ Özfen <goktugozfen@gmail.com>
2025-12-05 13:55:35 +0300

    add /query endpoint for invoices

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   typescript: {
     // ❗ Build sırasında tüm TypeScript hatalarını yok say → Vercel’in TS validation bug’ını bypass eder
     ignoreBuildErrors: true,
