eea4b4264887bdd0bcdcdecf8137766f812c640e
Göktuğ Özfen <goktugozfen@gmail.com>
2025-12-09 14:58:31 +0300

    feat: implement create and update invoice functionality with forms and actions

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,27 @@
+import { fetchCustomers } from "@/app/lib/data";
+import { customers } from "@/app/lib/placeholder-data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/create-form";
+import React from "react";
+
+export default async function Page() {
+
+    const customers = await fetchCustomers()
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoice", href: "/dashboard/invoices" },
+          {
+            label: "Create Invoices",
+            href: "/dashboard/invoices/create",
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+};
+
+
